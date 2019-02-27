6854d06789fb5226633ced7f6b57d1554705d9e6
mrtsven <zahharov@hotmail.com>
2019-02-27 14:37:37 +0100

    Fixed auto creating tables. Fixed maven. Test-data fetching.

--- src/main/java/Controllers/TestController.java
+++ src/main/java/Controllers/TestController.java
@@ -20,7 +20,7 @@ public class TestController extends HttpServlet {
 
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<h3>Supuuuuu...</h3>");
