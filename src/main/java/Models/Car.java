2711f91cf5e3d6bbce5601a39a7909691b4b9200
mrtsven <zahharov@hotmail.com>
2019-02-28 14:18:07 +0100

    Bean validation

--- src/main/java/Models/Car.java
+++ src/main/java/Models/Car.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Entity
@@ -11,14 +12,15 @@ public class Car implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
+    @NotNull(message = "The car name cannot be empty")
     private String carName;
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
